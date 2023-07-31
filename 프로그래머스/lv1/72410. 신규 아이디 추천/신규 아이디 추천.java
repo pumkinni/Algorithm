4d1cc363438b56b048177e38d636a70416186813
pumkinni <102509636+pumkinni@users.noreply.github.com>
2023-07-31 20:03:05 +0900

    [level 1] Title: 신규 아이디 추천, Time: 2.70 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -1,50 +1,25 @@
 class Solution {
     public String solution(String new_id) {
-        String answer = "";
         
         new_id = new_id.toLowerCase();
         
-        char[] step1_arr = new_id.toCharArray();
-        StringBuilder step2 = new StringBuilder();
-        for (char c : step1_arr) {
-            if ((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9') || c == '-' || c == '_' || c == '.') {
-                step2.append(c);
-            }
-        }
+        new_id = new_id.replaceAll("[^a-z0-9-_.]", "");
         
-        new_id = step2.toString();
+        new_id = new_id.replaceAll("[.]{2,}", ".");
         
-        while (new_id.contains("..")){
-            new_id = new_id.replace("..", ".");
-        }
-        
-        if (new_id.length() > 0 && new_id.charAt(0) == '.'){
-            new_id = new_id.substring(1, new_id.length());
-
-        }
-        
-        
-        if (new_id.length() > 0 && new_id.charAt(new_id.length()-1) == '.'){
-            new_id = new_id.substring(0, new_id.length()-1);
-        }
-        
+        new_id = new_id.replaceAll("^[.]|[.]$", "");
         
         if (new_id.length() == 0){
             new_id = "a";
         }
         
-        
-        if (new_id.length() >= 16) {
-            new_id = new_id.substring(0, 15);
-            
-            if (new_id.charAt(new_id.length()-1) == '.'){
-            new_id = new_id.substring(0, new_id.length()-1);
-            }
+        if (new_id.length() >= 15) {
+            new_id = new_id.substring(0,15);
+            new_id = new_id.replaceAll("[.]$", "");
         }
         
-        
-        while (new_id.length() <= 2){
-            new_id += new_id.charAt(new_id.length() - 1);
+        while(new_id.length() <= 2){
+            new_id += new_id.charAt(new_id.length()-1);
         }
         
         return new_id;
